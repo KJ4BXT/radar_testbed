5230c5e828b821200373284dfc78eae6bdd9ef6a
KJ4BXT <19842272+KJ4BXT@users.noreply.github.com>
2021-10-23 10:31:19 -0400

    Sim might be working

--- sim.py
+++ sim.py
@@ -19,11 +19,11 @@ import graphing as g
 fs = 1e6 # baseband sampling frequency. 10MHz startpoint
 dur = 0.4 # length of simulation
 freq = 0 # test signal freq. 0.3MHz CW startpoint
-BW = 0.5e6
+BW = 5e7
 #pulse_dur = 0.0005 # 10 ms startpoint
 #pulse_len = int(fs*pulse_dur)
 
-N = 1e5 # number of pulse samples
+N = 1e4 # number of pulse samples
 #pulse_t = np.arange(int(dur*fs))/fs
 pulse_t = np.arange(N)/fs
 
@@ -34,14 +34,14 @@ t = np.arange(0,dur,1/int(fs))
 source_sig = 0.5*np.exp(2.0j*np.pi*(freq+BW*pulse_t)*pulse_t)
 
 window = np.kaiser(len(source_sig), 10)
-pulse = source_sig#*window
+pulse = source_sig*window
 
-slice_dur = 0.4 # <- set this one
+slice_dur = 0.05 # <- set this one
 slice_len = int(slice_dur*fs)
 
 # Generate RX signal(s)
 noise = 1e-4
-targets = [[800e-6,1e-2],[864e-6,1.3e-3],[1804e-6, 3.4e-2]] # range (time), amplitude
+targets = [[800e-5,1e-1],[864e-6,1.3e-3],[1804e-6, 3.4e-2]] # range (time), amplitude
 RX = noise*np.random.rand(int(fs*dur)) # Generates base noise floor
 for i in targets:
     if ((i[0]*fs+len(pulse)) > len(t)):
@@ -50,27 +50,29 @@ for i in targets:
     RX = np.concatenate((RX[:int(i[0]*fs)],
                          RX[int(i[0]*fs)]+(pulse*i[1]),
                          RX[int(i[0]*fs)+len(pulse):]))
-
-dec = 10 #decimate
+'''
+dec = 1 #decimate
 # process received signals
 # TODO: convert to FFT processing with scipy.signal.correlate for speed
 DPC = np.zeros(len(t)) # Digital pulse compression vector
-for i in range(int(len(t)/dec)):
+for i in range(int(len(t)/dec-1)):
     i *= dec
-    print(i)
+    #print(i,'of ',int(len(t)/dec))
     if ((i>len(pulse)) and (i<(len(t)-len(pulse)-1))): # Only process full returns
     # if ((i>len(pulse)/2) and (i<(len(t)-len(pulse)/2-1))):
-        tmp = max(np.correlate(RX[i:i+len(pulse)],pulse))
+        tmp = np.correlate(RX[i:i+len(pulse)],pulse)
         #print(tmp)
-        if i%100 == 0:
-            print(i)
-        DPC[i] = tmp
+        if i%1000 == 0:
+            print(i,'of ',int(len(t)))
+        DPC[i] = sum(tmp.real)
+'''
 
+DPC = abs(np.correlate(RX,pulse))
 
 #to keep the log graph from throwing up, should probably do something better
 DPC += np.random.random(len(DPC))*1e-10
 
-# g.waterfall(pulse,256)
+g.waterfall(pulse,256)
 
 #Graph stuff
 plt.figure()
